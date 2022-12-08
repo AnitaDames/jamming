961333ac95e80ef1f9b2ed0f79f59632c2993575
TeacherAnita <teacheranitadames@gmail.com>
2022-12-08 17:52:51 -0500

    feat: add objects to mock playlist, search, tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,82 @@
-import React from 'react';
-import './App.css';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
+import React from "react";
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: "Perfect",
+          artist: "John Legend",
+          album: "The One",
+          id: "1",
+        },
+        {
+          name: "The Lamb",
+          artist: "Pentatonix",
+          album: "Christmas",
+          id: "2",
+        },
+        {
+          name: "Happy",
+          artist: "Feliz",
+          album: "Smile",
+          id: "3",
+        },
+        {
+          name: "Picture",
+          artist: "Ed Sherin",
+          album: "Grass",
+          id: "4",
+        },
+      ],
+      playlistName: "Most played",
+      playlistTracks: [
+        {
+          name: "Perfect",
+          artist: "John Legend",
+          album: "The One",
+          id: "1",
+        },
+        {
+          name: "Happy",
+          artist: "Feliz",
+          album: "Smile",
+          id: "3",
+        },
+        {
+          name: "Picture",
+          artist: "Ed Sherin",
+          album: "Grass",
+          id: "4",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <SearchBar />
-    <div className="App-playlist">
-    <SearchResults />
-    <Playlist />
-    </div>
-  </div>
-</div>
-    )
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-
 
 export default App;
