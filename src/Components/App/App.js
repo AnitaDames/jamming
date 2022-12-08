d430bdc1f7d0c27d35d02b0ccb1bad959e3148c4
TeacherAnita <teacheranitadames@gmail.com>
2022-12-08 10:46:41 -0500

    fix: correct typo and comment empty components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
+// import SearchResults from '../SearchResults/SearchResults';
+// import Playlist from '../Playlist/Playlist';
 
-class App extends React.Components {
+class App extends React.Component {
   render() {
     return (
       <div>
@@ -12,8 +12,8 @@ class App extends React.Components {
   <div className="App">
     <SearchBar />
     <div className="App-playlist">
-    <SearchResults />
-    <Playlist />
+    {/* <SearchResults /> */}
+    {/* <Playlist /> */}
     </div>
   </div>
 </div>
