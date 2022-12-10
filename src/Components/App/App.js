76af852aa69596317d1c9f71ef494f035252f0aa
TeacherAnita <teacheranitadames@gmail.com>
2022-12-09 21:29:14 -0500

    feat: create add-track function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,6 +7,7 @@ import Playlist from "../Playlist/Playlist";
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       searchResults: [
         {
@@ -56,6 +57,19 @@ class App extends React.Component {
         },
       ],
     };
+    
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let songs = this.state.playlistTracks;
+    if (songs.find((savedSongs) => savedSongs.id === track.id)) {
+      return;
+    }
+    songs.push(track);
+    this.setState({
+      playlistSongs: songs,
+    });
   }
 
   render() {
@@ -67,7 +81,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults}  onAdd={this.addTrack}/>
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
