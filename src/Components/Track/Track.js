23a54d5cbf015e44aa6ac457cfb2a207a61f0425
TeacherAnita <teacheranitadames@gmail.com>
2022-12-13 06:33:11 -0500

    feat: add onClick property

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -10,13 +10,16 @@ class Track extends React.Component {
     if (this.props.isRemoval) {
       return <button className="Track-action">-</button>;
     } else {
-      return <button className="Track-action ">+</button>;
+      return (
+        <button className="Track-action " onClick={this.addTrack}>
+          +
+        </button>
+      );
     }
   }
 
-  addTrack(){
+  addTrack() {
     this.props.onAdd(this.props.track);
-   
   }
 
   render() {
@@ -24,13 +27,14 @@ class Track extends React.Component {
       <div className="Track">
         <div className="Track-information">
           <h3>{this.props.track.name}</h3>
-          <p>{this.props.track.artist} | {this.props.track.album} </p>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}{" "}
+          </p>
         </div>
         {this.renderAction()}
       </div>
     );
   }
 }
-
 
 export default Track;
